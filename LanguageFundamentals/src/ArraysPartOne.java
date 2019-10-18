177328c00f653872bd04ff14539193d9a4e753ec
Girish Pawar <girish.pawar@icloud.com>
2019-10-18 10:48:26 +0530

    ArraysPartOne first checkin to LanguageFundamentals

---
+++ LanguageFundamentals/src/ArraysPartOne.java
@@ -0,0 +1,22 @@
+
+public class ArraysPartOne {
+
+	//one dimensional arry declaration
+	int[] x; //most meaning full declaration(recommended)
+	int []y;
+	int z[];
+	
+	//two dimensional array declaration
+	int[][] a; //most meaning full declaration(recommended)
+	int [][]b;
+	int c[][];
+	int[] d[];
+	int []e[];
+	int[] []f;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
